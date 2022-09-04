2ad7a871ee446afe8cf8722e3b58a3f900eaf8e9
Fatih Bağcıoğlu <fatih.bagcioglu@bilgeadamboost.com>
2022-09-04 23:33:54 +0300

    HomeViewModelHatası

---
+++ HopBurger/Models/HomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HopBurger.Models
+{
+    public class HomeViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public List<Item> Items { get; set; }
+    }
+}
